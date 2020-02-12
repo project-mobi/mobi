dffb90b2e26ce64e8fcafaa680a1c293aa52bc60
Fabian Volkers <fabianvolkers@gmail.com>
2020-02-12 10:08:16 +0100

    added mainframe, mass, messenger and monitoring docker-compose files with some data directories.

---
+++ mass/app/config/config.php
@@ -0,0 +1,23 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocpg820ubcpn',
+  'passwordsalt' => 'MtrycOZv+XmMq8+Z/6CHYzIl6CYN8y',
+  'secret' => 'G+AZDeHIs5ytOhd3+w2w0Cj9k3ykgEQTeJL/xv2kPQ7tgQg7',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => 'nextcloud.fabianvolkers.com',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '18.0.0.10',
+  'overwrite.cli.url' => 'http://nextcloud.fabianvolkers.com',
+  'dbname' => 'nextcloud_db',
+  'dbhost' => 'db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'sV7CbxGCtNkZWhC9PLlExJ04',
+  'installed' => true,
+);
