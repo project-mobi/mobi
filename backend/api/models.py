aa37bfc52feadda4bb86e54d9629e18df8fca2c7
Fabian Volkers <fabian.volkers@gmail.com>
2020-02-27 10:00:42 +0100

    added hosting plans as a dataclass

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,13 +1,22 @@
 # These are all the api resources
 
 from dataclasses import dataclass
+from typing import List, Dict
 
 @dataclass
-class Organisation:
+class HostingPlan:
+    id: int
     name: str
-
-
+    
 @dataclass
 class Service:
     name: str
+
+@dataclass
+class Organisation:
+    id: int
+    name: str
+    services: List[Service]
+
+
 
